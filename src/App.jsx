dcf43f14eed406a3d947362608101ae64c9327f9
Vera Mensah <veramensah2004@gmail.com>
2026-09-14 20:16:15 +0000

    Add working Add Item form with controlled inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import { useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState([
+  const [items, setItems] = useState([ 
     { itemName: "Apple", quantity: 10, price: 100 },
     { itemName: "Milk", quantity: 5, price: 80 },
   ]);
+    const [itemName, setItemName] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [price, setPrice] = useState("");
+    function handleAddItem() {
+      if (itemName === "") {
+        alert("Item name cannot be empty");
+        return;
+      }
+      setItems([...items, { itemName, quantity, price }]);
+      setItemName("");
+      setQuantity("");
+      setPrice("");
+    }
 
   return (
     <>
       <h1>My Shop Inventory</h1>
+      <input
+  type="text"
+  value={itemName}
+  onChange={(e) => setItemName(e.target.value)}
+  placeholder="Item name"
+  
+/>
+<input
+type="text"
+value={quantity}
+onChange={(e) => setQuantity(e.target.value)}
+placeholder="Quantity"
+/>
+<input
+type="text"
+value={price}
+onChange={(e) => setPrice(e.target.value)}
+placeholder="Price"
+/>
+<button onClick={handleAddItem}>Add Item</button>
       <table>
         <tr>
           <th>Item Name</th>
